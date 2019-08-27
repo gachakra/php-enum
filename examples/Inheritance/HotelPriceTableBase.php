e83584c98e33436f5c59a5cf1551a883a39bc9f8
Kangin Kim <gachakra.kangin@notonza.dev>
2019-08-27 17:06:52 +0900

    add example

---
+++ examples/Inheritance/HotelPriceTableBase.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: gachakra
+ * Date: 2019-08-05
+ * Time: 20:32
+ */
+
+declare(strict_types=1);
+
+
+namespace Gachakra\PhpEnum\Examples\Inheritance;
+
+use Gachakra\PhpEnum\Enum;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+/**
+ * @method static static SPECIAL_ROOM()
+ * @method static static GENERAL_ROOM()
+ */
+abstract class RoomPriceTypeBase extends Enum {
+
+    protected const SPECIAL_ROOM = 16200;
+    protected const GENERAL_ROOM = 12960;
+}
+
+/**
+ * @method static static DORMITORY()
+ */
+class SpringRoomPriceType extends RoomPriceTypeBase {
+
+    // add spring specific price type
+    protected const DORMITORY = 8640;
+}
+
+
+class SummerRoomPriceType extends RoomPriceTypeBase {
+
+    // override parent Enum prices
+    protected const SPECIAL_ROOM = 23760;
+    protected const GENERAL_ROOM = 21600;
+}
+
+/**
+ * @method static static DORMITORY()
+ */
+class AutumnRoomPriceType extends RoomPriceTypeBase {
+
+    // add autumn specific price type
+    protected const DORMITORY = 7560;
+}
+
+class WinterRoomPriceType extends RoomPriceTypeBase {
+
+    // nothing changes from base room price
+}
+
+
+{
+    $priceType = AutumnRoomPriceType::class;
+
+    assert($priceType::has('SPECIAL_ROOM'));
+    assert($priceType::has('GENERAL_ROOM'));
+    assert($priceType::has('DORMITORY'));
+
+
+    $priceType = WinterRoomPriceType::class;
+
+    assert($priceType::has('SPECIAL_ROOM'));
+    assert($priceType::has('GENERAL_ROOM'));
+    assert(!$priceType::has('DORMITORY'));
+}
+
+{
+    assert(SpringRoomPriceType::of('SPECIAL_ROOM')->value() === 16200);
+    assert(SummerRoomPriceType::of('SPECIAL_ROOM')->value() === 23760);
+
+    assert(SpringRoomPriceType::of('DORMITORY')->value() === 8640);
+    assert(AutumnRoomPriceType::of('DORMITORY')->value() === 7560);
+
+    assert(SpringRoomPriceType::of('GENERAL_ROOM')->value() === WinterRoomPriceType::of('GENERAL_ROOM')->value());
+}
